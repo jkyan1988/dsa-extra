c942c62f042b8a6a4985ca9cf044d0870658454c
James Kyan <james.kyan88@gmail.com>
2022-12-18 14:36:19 -0500

    first commit

---
+++ multiple_pointers_exercise.js
@@ -0,0 +1,34 @@
+// Multiple Pointers Pattern Exercise
+// Directions: Please solve the following problems in VS Code editor and submit a link to your 
+// GitHub project. 
+// Multiple Pointers Pattern Problem: 
+// Problem #1: Implement a function called countUniqueValues, which accepts a 
+// sorted array, and counts the unique values in the array. There can be 
+// negative numbers in the array, but it will always be sorted.
+// countUniqueValues([1,1,1,2]) // 2
+// countUniqueValues([1,2,3,4,5,5,5,6,6,7]) // 7
+// countUniqueValues([2,3,3,3,6,6,6,6]) // 3
+// Problem #2: 
+// Write a function called sumZero which accepts a sorted array of integers. The 
+// function should find the first pair where the sum is 0. Return an array that includes 
+// both values that sum to zero or undefined if a pair does not exist.
+// sumZero([-3, -2, -1, 0, 1, 2, 3]); // [-3, 3]
+// sumZero([-2, 0, 1, 3]); // undefined
+// sumZero([1, 2, 3]); // undefined
+const sumZero = (arr) => {
+
+}
+
+
+// Problem #3: 
+// Write a function called averagePair. Given a sorted array of integers and a target
+// average, determine if there is a pair of values in the array where the average of the
+// pair equals the target average. There may be more than one pair that matches the
+// average target.
+// averagePair([1, 2, 3], 2.5); // true
+// averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); // true
+// averagePair([-1, 0, 3, 4, 5, 6], 4.1); // false
+// averagePair([], 4); // false
+// const averagePair = (arr, val) => {
+// }
+
